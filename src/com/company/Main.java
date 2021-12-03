442612b26a30d872b59a68fee0f0299e39e97e1f
Mmarill <Max.henkesson@gmail.com>
2021-12-03 09:22:46 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,12 @@
 package com.company;
 
+import express.Express;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello PIM");
+
+        Express app = new Express();
     }
+
 }
