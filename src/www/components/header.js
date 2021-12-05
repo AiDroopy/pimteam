484378660066e13a083430156da494ffc3f46e7a
Mmarill <Max.henkesson@gmail.com>
2021-12-05 17:12:50 +0100

    Comments
    Added comments to code

--- src/www/components/header.js
+++ src/www/components/header.js
@@ -1,3 +1,4 @@
+// render header for application
 function renderHeader(){
     return `
     
--- src/www/main.js
+++ src/www/main.js
@@ -1,9 +1,12 @@
+
+//renders constant header from header.js
 document.querySelector('header').innerHTML = renderHeader();
 
 onhashchange = changePage();
 
 changePage();
 
+//change page on hashchange using switch case
 function changePage() {
 
     let page = location.hash.replace('#', '');
@@ -23,9 +26,10 @@ function changePage() {
         break;
 
         default: 
-        
+
+        // empty HTML for main
             document.querySelector('main').innerHTML = `<div class='form-container'></div>`
-        
+        // add HTML to main
             document.querySelector('.form-container').innerHTML =  `
                 <a id="hoover"><i class="far fa-sticky-note"></i><div class="formTxt">Add note<i class="fas fa-plus"></i></div></a>
                 <a id="hoover"><i class="fas fa-camera-retro"></i><div class="formTxt">Add image<i class="fas fa-plus"></i></div></a>
